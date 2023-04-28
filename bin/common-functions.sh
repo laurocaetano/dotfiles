93397d8370d978334a9d20fef1cfe2bdcd659765
Lauro Caetano <laurocaetano1@gmail.com>
2023-04-29 00:36:38 +0200

    Adapt scripts to use zsh instead of bash

--- bin/common-functions.sh
+++
@@ -1,5 +0,0 @@
-deleteAllBranchesAndKeepMaster() {
-  git checkout master
-  git pull origin master
-  git branch | grep -v "master" | xargs git branch -D
-}
--- install.sh
+++ install.sh
@@ -1,13 +1,7 @@
-#!/bin/sh
+ln -s -f ~/.dotfiles/zsh/zshrc ~/.zshrc
 
-mv ~/.bash_profile ~/.bash_profile_old
-ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+ln -s -f ~/.dotfiles/git/gitconfig ~/.gitconfig
 
-mv ~/.gitconfig ~/.gitconfig_old
-ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -s -f ~/.dotfiles/git/gitignore ~/.gitignore
 
-mv ~/.gitignore ~/.gitignore_old
-ln -s ~/.dotfiles/git/gitignore ~/.gitignore
-
-mv ~/.starship ~/.starship_old
-ln -s ~/.dotfiles/starship ~/.starship
+ln -s -f ~/.dotfiles/starship ~/.starship
